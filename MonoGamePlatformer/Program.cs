36eb0d9a7b29e063db51a787bdd1eb37b5110c74
Illia Ratkevych <illia.ratkevych@gmail.com>
2013-02-05 19:24:51 +0200

    Add basic source files.

---
+++ MonoGamePlatformer/Program.cs
@@ -0,0 +1,28 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+#endregion
+
+namespace MonoGamePlatformer
+{
+	/// <summary>
+	/// The main class.
+	/// </summary>
+	public static class Program
+	{
+		private static PlatformerGame game;
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			game = new PlatformerGame();
+			game.Run();
+		}
+	}
+}
